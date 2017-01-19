272c5b6e0b5dc0e4ebf3f9b11454c8d4b761a756
arkypita <arkypita@bergamo3.it>
2017-01-19 16:48:01 +0100

    changes

---
+++ ChordEditor/Forms/PendingChangesForm.Designer.cs
@@ -0,0 +1,50 @@
+﻿namespace ChordEditor.Forms
+{
+	partial class PendingChangesForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.SuspendLayout();
+			// 
+			// PendingChangesForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(870, 320);
+			this.DockAreas = ((ChordEditor.UserControls.DockingManager.DockAreas)((ChordEditor.UserControls.DockingManager.DockAreas.Float | ChordEditor.UserControls.DockingManager.DockAreas.DockBottom)));
+			this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.Name = "PendingChangesForm";
+			this.TabText = "Pending Changes";
+			this.Text = "Pending Changes";
+			this.ToolTipText = "Pending Changes";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+	}
+}
---
+++ ChordEditor/Forms/PendingChangesForm.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ChordEditor.Forms
+{
+	public partial class PendingChangesForm : ChordEditor.UserControls.DockingManager.DockContent
+	{
+		public PendingChangesForm()
+		{
+			InitializeComponent();
+		}
+	}
+}
