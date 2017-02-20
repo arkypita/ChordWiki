a2ff0d99f429d0a0c6cc1427ade55629ea74a863
arkypita <arkypita@bergamo3.it>
2017-02-20 15:25:15 +0100

    aaa

--- ChordEditor/Core/SongBook.cs
+++ ChordEditor/Core/SongBook.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Microsoft.Office.Interop.Word;
+
 namespace ChordEditor.Core
 {
 		public static class SongBook
@@ -47,108 +49,138 @@ namespace ChordEditor.Core
 						{
 								if (begin != null) begin(unsorted+indexed);
 
-								Microsoft.Office.Interop.Word.Application app = new Microsoft.Office.Interop.Word.Application();
-								Microsoft.Office.Interop.Word.Document doc = app.Documents.Open(System.IO.Path.GetFullPath("./Template/chordbook.docx"));
-
+								Application app = new Application();
+								Document doc = app.Documents.Open(System.IO.Path.GetFullPath("./Template/chordbook.docx"));
+								Template tpl = new Template(doc);
 
 								
-								foreach(KeyValuePair<string, JobDictionary.CategoryGroup> kvp in mList)
-								{
-										int count = 0;
-										foreach (ProcessedSheet ps in kvp.Value.Indexed)
-										{
-												ps.Process(doc);
-												if (progress != null)
-														progress(count++, ps.Sheet.Header);
-										}
-										foreach (ProcessedSheet ps in kvp.Value.Unsorted)
-										{
-												ps.Process(doc);
-												if (progress != null)
-														progress(count++, ps.Sheet.Header);
-										}
-								}
+								PreProcess(progress, doc, tpl);
+								//todo: implement sorting
+								Write(doc);
+
+								app.Dialogs[WdWordDialog.wdDialogFileSaveAs].Show();
+								doc.Close();
 
 								if (end != null) end();
 						}
 
+						private void Write(Document doc)
+						{
+								//foreach (KeyValuePair<string, JobDictionary.CategoryGroup> kvp in mList)
+								//{
+								//		//int count = 0;
+								//		foreach (ProcessedSheet ps in kvp.Value.Indexed)
+								//		{
+								//				ps.Write(doc);
+								//				//if (progress != null)
+								//				//		progress(count++, ps.Sheet.Header);
+								//		}
+								//		foreach (ProcessedSheet ps in kvp.Value.Unsorted)
+								//		{
+								//				ps.Write(doc);
+								//				//if (progress != null)
+								//				//		progress(count++, ps.Sheet.Header);
+								//		}
+								//}
+						}
 
+						private void PreProcess(JobProgress progress, Document doc, Template tpl)
+						{
+								foreach (KeyValuePair<string, JobDictionary.CategoryGroup> kvp in mList)
+								{
+										int count = 0;
+										foreach (ProcessedSheet ps in kvp.Value.Indexed)
+										{
+												ps.Analyze(doc, tpl);
+												if (progress != null)
+														progress(count++, ps.mSheet.Header);
+										}
+										foreach (ProcessedSheet ps in kvp.Value.Unsorted)
+										{
+												ps.Analyze(doc, tpl);
+												if (progress != null)
+														progress(count++, ps.mSheet.Header);
+										}
+								}
+						}
+
+						private void Sort()
+						{
+								//foreach (KeyValuePair<string, JobDictionary.CategoryGroup> kvp in mList)
+								//{
+								//		int count = 0;
+								//		foreach (ProcessedSheet ps in kvp.Value.Indexed)
+								//		{
+								//				ps.Analyze(tpl);
+								//				if (progress != null)
+								//						progress(count++, ps.Sheet.Header);
+								//		}
+								//		foreach (ProcessedSheet ps in kvp.Value.Unsorted)
+								//		{
+								//				ps.Analyze(tpl);
+								//				if (progress != null)
+								//						progress(count++, ps.Sheet.Header);
+								//		}
+								//}
+						}
+
+
+						private class Template
+						{
+								public Style TitleStyle;
+								public Style ArtistStyle;
+								public Style ChordStyle;
+								public Style ChorusStyle;
+								public Style StropheStyle;
+
+								public PageSetup Page;
+
+								public Template(Document doc)
+								{
+										TitleStyle = doc.Styles["SongTitle"];
+										ArtistStyle = doc.Styles["SongArtist"];
+										ChordStyle = doc.Styles["Chords"];
+										ChorusStyle = doc.Styles["Chorus"];
+										StropheStyle = doc.Styles["Strophe"];
+
+										Page = doc.PageSetup;
+								}
+						}
 
 						private class ProcessedSheet
 						{
-								internal Sheet Sheet;
-								List<ISongElement> mList = new List<ISongElement>();
-								double FinalHeight = 0.0;
-
-								private interface ISongElement
-								{
-										
-								}
-
-								private class TitleElement : ISongElement
-								{
-										private string p;
-
-										public TitleElement(string p)
-										{this.p = p;}
-								}
-
-								private class ArtistElement : ISongElement
-								{
-										private string p;
-
-										public ArtistElement(string p)
-										{
-												// TODO: Complete member initialization
-												this.p = p;
-										}
-								}
-
-								private class ChorusElement : ISongElement
-								{ }
-
-								private class StropheElement : ISongElement
-								{
-										private string line;
-
-										public StropheElement(string line)
-										{
-												// TODO: Complete member initialization
-												this.line = line;
-										}
-								}
-
-								private class ChordsElement : ISongElement
-								{ }
+								internal Sheet mSheet;
+								List<Paragraph> mContent = new List<Paragraph>();
 
 								public ProcessedSheet(SheetHeader sh)
+								{mSheet = new Sheet(sh.FileName);}
+
+								internal void Analyze(Document doc, Template tpl)
 								{
-										Sheet = new Sheet(sh.FileName);
-								}
+										mSheet.ReloadFile();
 
+										AddParagraph(doc, tpl.TitleStyle, mSheet.Header.Title);
+										if (mSheet.Header.Artist != null)
+												AddParagraph(doc, tpl.ArtistStyle, mSheet.Header.Artist);
 
-								internal void Process(Microsoft.Office.Interop.Word.Document doc)
-								{
-										Sheet.ReloadFile();
-
-										mList.Add(new TitleElement(Sheet.Header.Title));
-										FinalHeight += doc.Styles["SongTitle"].Font.Size;
-
-										if (Sheet.Header.Artist != null)
-										{
-												mList.Add(new ArtistElement(Sheet.Header.Artist));
-												FinalHeight += doc.Styles["SongArtist"].Font.Size;
-										}
-
-										using (var reader = new System.IO.StringReader(Sheet.Content))
+										using (var reader = new System.IO.StringReader(mSheet.Content))
 										{
 												for (string line = reader.ReadLine(); line != null; line = reader.ReadLine())
 												{
-														mList.Add(new StropheElement(line));
-														FinalHeight += 1.0;
+														AddParagraph(doc, tpl.StropheStyle, line);
 												}
 										}
 								}
+
+								private void AddParagraph(Document doc, Style style, string content)
+								{
+										Paragraph par = doc.Content.Paragraphs.Add();
+										par.Range.Text = content;
+										par.set_Style(style);
+										//mContent.Add(par);
+										par.Range.InsertParagraphBefore();
+								}
+
 
 						}
 
@@ -163,7 +195,7 @@ namespace ChordEditor.Core
 
 						foreach (SheetHeader sh in SheetDB.List)
 						{
-								if (!sh.Deletable && /*sh.Progress >= SheetHeader.SheetProgress.Reviewed &&*/ sh.SheetCategory != null)
+								if (!sh.Deletable && sh.Progress >= SheetHeader.SheetProgress.Locked && sh.SheetCategory != null)
 										job.Add(sh, opt);
 						}
 
